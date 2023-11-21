84e49f420f1a94319c96e38233344e6e4e95824a
ducmanh1313 <vuducmanh1313@gmail.com>
2023-11-21 16:12:01 +0700

    slide 13

---
+++ php/h.php
@@ -0,0 +1,9 @@
+<?php
+    echo 'Xin chao Vu Duc Manh';
+    echo 'My first PHP file';
+    echo '<h3> Hello World!</h3>';
+    echo '<select>';
+    echo "<option>1</option>";
+    echo "<option>2</option>";
+    echo "</select>";
+?>
