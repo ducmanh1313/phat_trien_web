2544b220c72e11fb03e105d962d757baaf3b7f0e
ducmanh1313 <vuducmanh1313@gmail.com>
2023-12-06 13:24:29 +0700

    slide 20

---
+++ php/P3/slide20.php
@@ -0,0 +1,6 @@
+<?php
+$items = array('img1.png', 'img2.png');
+echo "Vũ Đức Mạnh-2121050430";
+foreach ($items as $item) {
+    echo "<img src = 'images/$item' width =150px height=150px alt='$item'>";
+}
