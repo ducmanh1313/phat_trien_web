5ee11b0c641e5130d2df1b1b12087e075a275ff5
ducmanh1313 <vuducmanh1313@gmail.com>
2023-12-08 00:06:49 +0700

    slide30

---
+++ php/P2/slide30.php
@@ -0,0 +1,7 @@
+<?php
+$exchangeRate = array("USD" => 22300, "EUR" => 27300, "SGD" => 17000);
+echo "<h1>Vu Duc Manh</h1>";
+echo "USD:" . $exchangeRate['USD'] . "<br>";
+echo "EUR:" . $exchangeRate['EUR'] . "<br>";
+echo "SGD:" . $exchangeRate['SGD'] . "<br>";
+echo "JPY:" . $exchangeRate['JPY'] . "<br>";
