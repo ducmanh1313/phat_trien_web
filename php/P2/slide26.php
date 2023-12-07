ba2cbb1c37d5aef8e2ed554b52fb8db36b971c20
ducmanh1313 <vuducmanh1313@gmail.com>
2023-12-08 00:05:56 +0700

    slide28

---
+++ php/P2/slide26.php
@@ -0,0 +1,9 @@
+<?php
+    $exchange = array(22300, 27300, 17000, 120);
+    echo "<h2>Vu Duc Manh </h2>";
+    echo "<h3>Exchange rate to VNĐ</h3>";
+    echo "USD: $exchange[0] <br>";
+    echo "EUR: $exchange[1] <br>";
+    echo "AUD: $exchange[2] <br>";
+    echo "JPY: $exchange[3] <br>";
+    ?>
---
+++ php/P2/slide28.php
@@ -0,0 +1,13 @@
+<?php
+$exchangeRate = array(22300, 27300, 1700, 120);
+$exchangeRate[] = 600;
+$exchangeRate[] = 16000;
+$exchangeRate[0] = 22200;
+echo "<h1> Vu Duc MAnh</h1></h1>";
+echo "USD: $exchange[0] <br>";
+echo "EUR: $exchange[1] <br>";
+echo "AUD: $exchange[2] <br>";
+echo "JPY: $exchange[3] <br>";
+echo "CHN: $exchange[4] <br>";
+echo "SGD: $exchange[5] <br>";
+?>
