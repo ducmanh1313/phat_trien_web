d7a02518952d21fa588d08b84d7573207dc06f6a
ducmanh1313 <vuducmanh1313@gmail.com>
2023-12-08 00:19:21 +0700

    slide40

--- php/MySQL/slide40.php
+++ php/MySQL/slide40.php
@@ -1,4 +1,13 @@
-<?php
-   $servername = "locahost";
-   $username ="root";
-   $password
+<?
+$servername = "localhost";
+$username = "root";
+$password = " ";
+$db = "myDB";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully <br>";
+?>
