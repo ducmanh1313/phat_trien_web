480e4c7d4d989060cbdc48d6d1c7907f34549c00
ducmanh1313 <vuducmanh1313@gmail.com>
2023-12-07 23:24:45 +0700

    thuchanh1

---
+++ php/P2/Thuchanh2.php
@@ -0,0 +1,18 @@
+<?php
+$USD = 23000;
+$AUD = 17000;
+$JPY = 200;
+$EUR = 27000;
+$amount = $_GET['amount'];
+$currency = $_GET['currency'];
+$VND = 0;
+if ($currency == "USD") {
+    $VND = $amount * $USD;
+} elseif ($currency == "AUD") {
+    $VND = $amount * $AUD;
+} elseif ($currency == "JPY") {
+    $VND = $amount * $JPY;
+} else {
+    $VND = $amount * $EUR;
+}
+echo "$amount $currency = $VND VND";
